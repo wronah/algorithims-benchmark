f678f6f25f3409965f881f4b0f91c88ab4ce58c5
wronah <hubertwro7@gmail.com>
2025-04-01 21:48:02 +0200

    add value generators

---
+++ algorithims-benchmark/Generators.cs
@@ -0,0 +1,29 @@
+﻿namespace algorithims_benchmark
+{
+    public static class Generators
+    {
+        public static int[] GenerateRandom(int size, int minVal, int maxVal)
+        {
+            int[] a = new int[size];
+
+            var random = new Random();
+            for(int i = 0; i < size; i++)
+            {
+                a[i] = random.Next(minVal, maxVal);
+            } 
+            return a;
+        }
+        public static int[] GenerateSorted(int size, int minVal, int maxVal)
+        {
+            int[] a = GenerateRandom(size, minVal, maxVal);
+            Array.Sort(a);
+            return a;
+        }
+        public static int[] GenerateReversed(int size, int minVal, int maxVal)
+        {
+            int[] a = GenerateSorted(size, minVal, maxVal);
+            Array.Reverse(a);
+            return a;
+        }
+    }
+}
